653a89a823a3f722fa68af3cf556811d9b3ff009
Kirill <kirillbaranovskiy01@gmail.com>
2026-08-29 19:04:17 +0300

    BLOG-003 COMPLETED

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+    text-align: center;
+`;
+
+function App() {
+    return (
+        <>
+            <Div>
+                <i className="fa fa-bath"></i>
+                456
+            </Div>
+        </>
+    );
+}
+
+export default App;
