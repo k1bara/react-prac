421baaf132dafa6f504c03a1489c4da5d4f7373b
Kirill <kirillbaranovskiy01@gmail.com>
2026-08-29 23:15:51 +0300

    Реализовать шапку

--- src/blog.jsx
+++ src/blog.jsx
@@ -1,6 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-
+import { Header } from "./components";
 import styled from "styled-components";
+
+const AppColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 1000px;
+    min-height: 100%;
+    margin: 0 auto;
+    background-color: #fff;
+`;
 
 const Content = styled.div`
     padding: 120px 0;
@@ -9,11 +19,12 @@ const Content = styled.div`
 const H2 = styled.h2`
     text-align: center;
 `;
-const Header = () => <div>Шапка</div>;
+
 const Footer = () => <div>Футер</div>;
+
 function Blog() {
     return (
-        <>
+        <AppColumn>
             <Header />
             <Content>
                 <H2>Контент страницы</H2>
@@ -34,7 +45,7 @@ function Blog() {
                 </Routes>
             </Content>
             <Footer />
-        </>
+        </AppColumn>
     );
 }
 
---
+++ src/components/icon/icon.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react-refresh/only-export-components */
+import styled from "styled-components";
+
+const IconContainer = ({ className, id }) => (
+    <div className={className}>
+        <i className={`fa ${id}`} aria-hidden="true"></i>
+    </div>
+);
+
+export const Icon = styled(IconContainer)`
+    font-size: ${({ size = "24px" }) => size};
+    margin: ${({ margin = "0" }) => margin};
+`;
